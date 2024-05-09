63d68b15c38d4131af5218e399125e69038b5994
KaiSheng Cheng <wo4monkey@sina.com>
2024-05-08 21:00:04 -0400

    final

--- LeetCode_9.py
+++ LeetCode_9.py
@@ -4,10 +4,8 @@ def isPalindrome(x):
         :rtype: bool
         """
         temp = x
-        if (x == 0):
-            return True
             
-        if (x < 0 or x % 10 == 0):
+        if (x < 0):
             return False
 
         rev = 0

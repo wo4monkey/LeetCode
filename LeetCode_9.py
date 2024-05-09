4cb7235492a5a679acc99b1bedc8e5cecacaca41
KaiSheng Cheng <wo4monkey@sina.com>
2024-05-08 20:52:56 -0400

    added condition and fixed i

--- LeetCode_9.py
+++ LeetCode_9.py
@@ -4,17 +4,21 @@ def isPalindrome(x):
         :rtype: bool
         """
         temp = x
-        if (x<0):
+        if (x == 0):
+            return True
+            
+        if (x < 0 or x % 10 == 0):
             return False
+
         rev = 0
-        i = 0
+        i = len(str(x)) - 1
         while (temp>0):
             rev += (temp % 10) * (10 ** i)
             temp = temp // 10
-            i += 1
+            i -= 1
         return (rev == x)
 
 def main():
-    print (isPalindrome(121))
+    print (isPalindrome(123))
 
 main()
